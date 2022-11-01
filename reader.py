40f0bab4f3cc8652584fc953954a06dfe9c97782
Luis Nunes <luis.filipe@pucrs.br>
2022-11-01 14:38:34 -0300

    Primeiro Commit. parte do código que faz a leitura do arquivo através do input do usuário está pronta, assim como a parte que identifica o cabeçário e o utiliza no menu para o usuário.
    próximo update deve trazer a parte do filtro de fato do código.

---
+++ reader.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+#data = pd.read_csv("myFile0.csv")
+#print(data) # debug
+
+def reader_start():
+    print("código para extração de dados do arquivo CSV")
+    arqName = input("digite o nome do arquivo desejado:")
+    arqToFind = arqName + ".csv"
+    try:
+        data = pd.read_csv(arqToFind)
+    except:
+        print("Ocorreu um erro!")
+        #error handling TODO
+    else: 
+        print("Arquivo encontrado, leitura completa!")
+        df = data
+        df.columns = list(df)  # to put header inside data frame 
+        #print(df)
+        headers = list(df.columns) # to read header from data frame
+        #print(var)
+        reader_menu(data, headers)
+def reader_menu(data, headers):
+    
+    print("Escolha qual campo a pesquisa será feita")
+    n = 1
+    for header in headers:
+        print("Digite {number} para selecionar o campo '{header}' para a pesquisa.".format(number = n, header = header))
+        n += 1
+    #print(data)
+    field = input("Opção dessejada:")
+    if field == int(1):
+        search_value = input("Digite o valor desejado:")
+        
+    elif field == int(1):
+    elif field == int(1):
+    elif field == int(1):
+    elif field == int(1):
+    elif field == int(1):
+    else:
+reader_start()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+import panda as pd
+data = pd.read_csv("myFile0.csv")
+data
